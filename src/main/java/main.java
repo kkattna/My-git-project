5f5f76d4e32a732bd44eb49b41b7913dbb74d3c7
Kasia <kasia.kochaniewicz@gmail.com>
2025-08-17 20:51:33 +0200

    Change text + comments

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,10 @@
 public class main {
     public static void main(String[] args) {
-        print("Hello World");
+        print("Hello Minions");
     }
     public static void print(String text){
         System.out.println(text);
     }
+
+    //setters + getters text
 }
